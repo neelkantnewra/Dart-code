3dd3d2236e07095d5462449d3291b52b87050eeb
Neelkant Newra <63470232+neelkantnewra@users.noreply.github.com>
2020-08-26 06:00:22 +0100

    Integer input

---
+++ input_int.dart
@@ -0,0 +1,17 @@
+//taking integer as input
+
+//importing dart:io file
+
+import 'dart:io';
+
+void main() {
+  // print('Enter Your age: ');
+  //putting cursor in same line
+  stdout.write('Enter youe age: ');
+  //reading the age
+  
+  int age = int.parse(stdin.readLineSync());
+  //printing the age
+  print('You are $age year old');
+  
+}
